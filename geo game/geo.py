70ae4db8d5210d146dd71ff11e5cb56d7f735d13
Daniel Otuo-Acheampong <danielotuoach@gmail.com>
2023-10-21 17:41:05 -0400

    Added a class named Rectangle and refactored code

--- geo game/geo.py
+++ geo game/geo.py
@@ -1,24 +1,54 @@
+from random import randint
+
+
 class Point:
     def __init__(self, x, y):
         # Initialize Point object with x and y coordinates
         self.x = x
         self.y = y
-    
-    def inside_rectangle(self, lowleft, upright):
+
+    def inside_rectangle(self, rectangle):
         # Check if the Point is within the specified rectangle
-        if lowleft[0] < self.x < upright[0] \
-        and lowleft[1] < self.y < upright[1]:
+        if (
+            rectangle.point1.x < self.x < rectangle.point2.x
+            and rectangle.point1.y < self.y < rectangle.point2.y
+        ):
             return True
         else:
             return False
-    
-# Create a Point object with coordinates (4, 5)
-point1 = Point(4, 5)
-
-# Check if point1 is inside the rectangle defined by (2, 3) and (6, 8)
-is_inside = point1.inside_rectangle((2, 3), (6, 8))
-print(is_inside)  # Print the result (True or False) to the console
 
 
+class Rectangle:
+    # Initialize Rectangle object with two corner points
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def area(self):
+        # Calculate the area of the rectangle
+        return (self.point2.x - self.point1.x) * (self.point2.y - self.point1.y)
 
 
+# Create a random rectangle with corner points
+rectangle = Rectangle(
+    Point(randint(0, 9), randint(0, 9)), Point(randint(10, 19), randint(10, 19))
+)
+
+print(
+    "Rectangle Coordinates: ",
+    rectangle.point1.x,
+    ",",
+    rectangle.point1.y,
+    "and",
+    rectangle.point2.x,
+    ",",
+    rectangle.point2.y,
+)
+
+# Get the user's guessed point and area for the rectangle
+user_point = Point(float(input("Guess X: ")), float(input("Guess Y: ")))
+user_area = float(input("Guess area of the rectangle: "))
+
+print("Your point was inside rectangle: ", user_point.inside_rectangle(rectangle))
+
+print("Your area was of by: ", rectangle.area() - user_area)
